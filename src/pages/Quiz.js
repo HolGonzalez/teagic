806f1888bccaa7ea542c38cbbfeb86ed15d16bef
Holman Gonzalez <holgonzalez94@gmail.com>
2020-06-17 21:53:50 -0500

    Mejorando Logica Quiz Se mejoro la logica de cada pregunta del quiz Se desactiva el resto de opciones al selecionar una opción de la pregunta

--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -1,7 +1,6 @@
 import React, {Component, Fragment} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bulma/css/bulma.css'
-import Question from '../components/Question'
 
 
 class Quiz extends Component {
@@ -19,40 +18,78 @@ class Quiz extends Component {
     }
 
     Left(e){
+        this.ValidateOption(e)
         this.setState({
             left: this.state.left + parseInt(e.target.value)
         })
     }
 
     Right(e){
+        this.ValidateOption(e)
         this.setState({
             right: this.state.right + parseInt(e.target.value)
         })
     }
 
+    ValidateOption(e){
+        var number = document.getElementsByName(e.target.name);
+        for(var i = 0; i < number.length; i++){   
+            number[i].disabled = true
+        }
+    }
 
     render() {
+        console.log(this.state.left, this.state.right)
+
         return (
-            <div className="container p-2 mx-auto">
-                <p className="subtitle is-3">Pregunta Uno</p>
-                <div class="control">
-                    <label className="radio">
-                        <input type="radio" name="answer" value="1"
-                            onChange={ this.Left}/> Izquierda
-                    </label>
-                    <label className="radio">
-                        <input type="radio" name="answer" value="1"
-                            onChange={ this.Right}/> Derecha
-                    </label>
+            <div className="container-md p-2 col-6">
+                <h2 className="title is-2 has-text-centered-desktop">Test cerebro</h2>
+                <div className="row row-cols-1 box">
+                <Question 
+                    title="Tiendo a estar más frecuentemente:" 
+                    numberQuestion="one"
+                    optionLeft=" Tenso y preocupado"
+                    optionRight=" Relajado y despreocupado"
+                    changeLeft={this.Left}
+                    changeRight={this.Right}
+                ></Question>
+                <Question 
+                    title="Pregunta numero 2:" 
+                    numberQuestion="two"
+                    optionLeft=" Tenso y preocupado"
+                    optionRight=" Relajado y despreocupado"
+                    changeLeft={this.Left}
+                    changeRight={this.Right}
+                ></Question> 
+                <Question 
+                    title="Pregunta numero 3:" 
+                    numberQuestion="three"
+                    optionLeft=" Tenso y preocupado"
+                    optionRight=" Relajado y despreocupado"
+                    changeLeft={this.Left}
+                    changeRight={this.Right}
+                ></Question> 
+            </div>
+                <div class="row row-cols-1">
+                    <input className="button is-link" type="submit" value="Enviar"></input>
                 </div>
-                <div class="control">
-                        <input className="button is-link" type="submit" value="Enviar"></input>
-                </div>
-                    <p>{this.state.left}</p>
-                    <p>{this.state.right}</p>
             </div>
         )
     }
 }
+
+const Question = ({title, numberQuestion, optionLeft, optionRight, changeLeft, changeRight}) => (
+    <div class="control mt-4 mb-4 p-2">
+        <p><strong>{title}</strong></p>
+        <label className="radio">
+            <input type="radio" name={numberQuestion} value="1"
+                onChange={changeLeft}/>{optionLeft}
+        </label>
+        <label className="radio">
+            <input type="radio" name={numberQuestion} value="1"
+                onChange={changeRight}/>{optionRight}
+        </label>
+    </div> 
+)
 
 export default Quiz
