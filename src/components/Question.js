78c0ed63d09a381a459f8ec96918eaa052d06780
Holman Gonzalez <holgonzalez94@gmail.com>
2020-06-17 21:56:40 -0500

    Componente Question Se creo el codigo correspondiente para crear cada pregunta

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,19 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bulma/css/bulma.css'
 
-const Question = () => (
-    <Fragment></Fragment>
+const Question = ({title, numberQuestion, optionLeft, optionRight, changeLeft, changeRight}) => (
+    <div class="control mt-4 mb-4 p-2">
+        <p><strong>{title}</strong></p>
+        <label className="radio">
+            <input type="radio" name={numberQuestion} value="1"
+                onChange={changeLeft}/>{optionLeft}
+        </label>
+        <label className="radio">
+            <input type="radio" name={numberQuestion} value="1"
+                onChange={changeRight}/>{optionRight}
+        </label>
+    </div> 
 )
 
 export default Question
--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -1,7 +1,7 @@
 import React, {Component, Fragment} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bulma/css/bulma.css'
-
+import Question from '../components/Question'
 
 class Quiz extends Component {
 
@@ -77,19 +77,5 @@ class Quiz extends Component {
         )
     }
 }
-
-const Question = ({title, numberQuestion, optionLeft, optionRight, changeLeft, changeRight}) => (
-    <div class="control mt-4 mb-4 p-2">
-        <p><strong>{title}</strong></p>
-        <label className="radio">
-            <input type="radio" name={numberQuestion} value="1"
-                onChange={changeLeft}/>{optionLeft}
-        </label>
-        <label className="radio">
-            <input type="radio" name={numberQuestion} value="1"
-                onChange={changeRight}/>{optionRight}
-        </label>
-    </div> 
-)
 
 export default Quiz
