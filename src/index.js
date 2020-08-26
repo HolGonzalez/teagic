04fa63b12bcf5e939693d5b5a48cf5f020dd6f37
Holman Gonzalez <holgonzalez94@gmail.com>
2020-08-26 17:46:58 -0500

    Correcion de importación serviceWorker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
-serviceWorkerimport * as serviceWorker from './serviceWorker'
+import * as serviceWorker from './serviceWorker'
 
 const root = document.getElementById('root')
 
