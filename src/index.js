7043216799e99f1107b859e1faf9891b4ce3a670
Holman Gonzalez <holgonzalez94@gmail.com>
2020-08-26 17:41:50 -0500

    Activacion del serviceWorker
    Se activo desde el archivo Index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
+serviceWorkerimport * as serviceWorker from './serviceWorker'
+
 const root = document.getElementById('root')
 
-
 ReactDOM.render(<App/>,root);
+serviceWorker.register()
